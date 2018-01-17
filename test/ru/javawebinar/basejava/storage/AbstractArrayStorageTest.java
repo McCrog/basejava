91c933d3a24fb379c9a5973accc033ae017f813d
McCrog <pit_mib@bk.ru>
2018-01-17 21:27:34 +0300

    Lesson 4. Added tests

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -3,37 +3,85 @@ package ru.javawebinar.basejava.storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 public abstract class AbstractArrayStorageTest {
-    private Storage storage = new ArrayStorage();
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
+    protected Storage storage;
+    protected static final String UUID_1 = "uuid1";
+    protected static final String UUID_2 = "uuid2";
+    protected static final String UUID_3 = "uuid3";
+
+    protected String[] unsortedUuidArray = {UUID_3, UUID_1, UUID_2};
+    protected String[] sortedUuidArray = {UUID_1, UUID_2, UUID_3};
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @Before
     public void setUp() {
         storage.clear();
+        storage.save(new Resume(UUID_3));
         storage.save(new Resume(UUID_1));
         storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void clear() {
+        storage.clear();
+        storage.get(UUID_1);
+        storage.get(UUID_2);
+        storage.get(UUID_3);
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() {
+        storage.update(new Resume(UUID_1));
+        storage.get(UUID_1);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateotExist() {
+        storage.update(new Resume("dummy"));
     }
 
     @Test
     public void save() {
+        storage.save(new Resume("1"));
+        storage.save(new Resume("-2"));
+        storage.save(new Resume("Save"));
+        storage.save(new Resume("Сохранение"));
+
+        storage.get("1");
+        storage.get("-2");
+        storage.get("Save");
+        storage.get("Сохранение");
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExist() {
+        storage.save(new Resume(UUID_1));
+    }
+
+    @Test(expected = StorageException.class)
+    public void checkStorageOverflow() {
+        int storageLength = ((AbstractArrayStorage) storage).storage.length;
+        Assert.assertEquals(storageLength, 10000);
+
+        for (int i = 3; i < storageLength + 1; i++) {
+            storage.save(new Resume(UUID_1 + i));
+        }
     }
 
     @Test
     public void get() {
+        storage.get(UUID_1);
+        storage.get(UUID_2);
+        storage.get(UUID_3);
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -41,16 +89,32 @@ public abstract class AbstractArrayStorageTest {
         storage.get("dummy");
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
+        storage.delete(UUID_3);
+        storage.get(UUID_3);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() {
+        storage.get("dummy");
     }
 
     @Test
     public void getAll() {
+        Resume[] resumes = storage.getAll();
+        Assert.assertNotNull(resumes);
+        Assert.assertEquals(3, resumes.length);
     }
 
     @Test
     public void size() {
         Assert.assertEquals(3, storage.size());
     }
+
+    @Test
+    public abstract void checkOrderSaveToStorage();
+
+    @Test
+    public abstract void checkOrderDeleteFromStorage();
 }
--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,5 +1,29 @@
 package ru.javawebinar.basejava.storage;
 
+import org.junit.Assert;
+import ru.javawebinar.basejava.model.Resume;
+
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+
+
+    @Override
+    public void checkOrderSaveToStorage() {
+        Resume[] resumes = storage.getAll();
+        for (int i = 0; i < resumes.length; i++) {
+            String uuid = resumes[i].getUuid();
+            Assert.assertEquals(uuid, unsortedUuidArray[i]);
+        }
+    }
+
+    @Override
+    public void checkOrderDeleteFromStorage() {
+        storage.delete(UUID_3);
+        Resume[] resumes = storage.getAll();
+        String uuid = resumes[0].getUuid();
+        Assert.assertEquals(uuid, UUID_2);
+    }
 }
