492f03c39b01bf9ca62aabbd4f538cd4b68862a4
McCrog <pit_mib@bk.ru>
2018-01-26 22:37:02 +0300

    Lesson 6. Added tests for the List and Map storage

---
+++ test/ru/javawebinar/basejava/storage/ListStorageTest.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.basejava.storage;
+
+public class ListStorageTest extends AbstractArrayStorageTest {
+
+    public ListStorageTest() {
+        super(new ListStorage());
+    }
+
+    @Override
+    public void checkOrderSaveToStorage() {
+
+    }
+
+    @Override
+    public void checkOrderDeleteFromStorage() {
+
+    }
+}
---
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.basejava.storage;
+
+public class MapStorageTest extends AbstractArrayStorageTest {
+
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+
+    @Override
+    public void checkOrderSaveToStorage() {
+
+    }
+
+    @Override
+    public void checkOrderDeleteFromStorage() {
+
+    }
+}
