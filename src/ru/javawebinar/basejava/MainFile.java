5017aacc9019ed95e50f84996f3a3c6f3aa525ab
McCrog <pit_mib@bk.ru>
2018-02-07 23:30:08 +0300

    Lesson 08. Home work. Added recursive crawling and output of file names in directories and subdirectories (root directory - project)

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -3,6 +3,7 @@ package ru.javawebinar.basejava;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
@@ -25,6 +26,25 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        File baseDirectory = new File("./");
+
+        recursiveBypass(baseDirectory);
+    }
+
+    private static void recursiveBypass(File file) {
+        for (File childFile : Objects.requireNonNull(file.listFiles())) {
+            if (childFile.isDirectory()) {
+                if (childFile.list() != null) {
+                    for (File name : childFile.listFiles()) {
+                        if (!name.isDirectory()) {
+                            System.out.println(name.getName());
+                        }
+                    }
+                }
+                recursiveBypass(new File(childFile.getPath()));
+            }
         }
     }
 }
