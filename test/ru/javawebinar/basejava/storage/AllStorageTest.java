b875449d33639cd619b1f189da2c023e37517815
McCrog <pit_mib@bk.ru>
2018-02-18 18:34:19 +0300

    Lesson10 DataStream

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -2,6 +2,7 @@ package ru.javawebinar.basejava.storage;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import ru.javawebinar.basejava.storage.serializer.DataStreamSerializer;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
@@ -14,7 +15,8 @@ import org.junit.runners.Suite;
                 ObjectFileStorageTest.class,
                 ObjectPathStorageTest.class,
                 XmlPathStorageTest.class,
-                JsonPathStorageTest.class
+                JsonPathStorageTest.class,
+                DataStreamSerializer.class
         })
 public class AllStorageTest {
 }
